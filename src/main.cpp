92dba564b44a7e37748d0016f2d49010ec02264e
infopek <10saero01@gmail.com>
2024-08-28 17:01:39 +0200

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "./models/list_node.h"
+
+#include <iostream>
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ListNode* deleteDuplicates(ListNode* head)
+{
+
+}
+
+int main()
+{
+    ListNode node1(1);
+}
