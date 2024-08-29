e29f61c8be684f9b522f74e73e18559f5c34dd98
infopek <10saero01@gmail.com>
2024-08-29 22:09:57 +0200

    Solve three more problems

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,183 @@
 #include "./models/list_node.h"
+#include "./models/tree_node.h"
 
+#include <algorithm>
 #include <iostream>
 #include <utility>
+#include <string>
 #include <vector>
 
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-ListNode* deleteDuplicates(ListNode* head)
+namespace p83
 {
-    ListNode* curr = head;
-    while (curr)
-    {
-        while (curr->next && curr->next->val == curr->val)
-        {
-            // Duplicate found -> unlink next node
-            curr->next = curr->next->next;
-        }
+	ListNode* deleteDuplicates(ListNode* head)
+	{
+		ListNode* curr = head;
+		while (curr)
+		{
+			while (curr->next && curr->next->val == curr->val)
+			{
+				// Duplicate found -> unlink next node
+				curr->next = curr->next->next;
+			}
 
-        curr = curr->next;
-    }
+			curr = curr->next;
+		}
 
-    return head;
+		return head;
+	}
 }
 
 // https://leetcode.com/problems/merge-sorted-array/
-void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) 
+namespace p88
 {
-    std::vector<int> merged(static_cast<size_t>(m + n));
+	void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
+	{
+		std::vector<int> merged(static_cast<size_t>(m + n));
 
-    size_t i = 0;
-    size_t j = 0;
-    size_t mergedIdx = 0;
-    while (i < m && j < n)
-    {
-        if (nums1[i] < nums2[j])
-        {
-            merged[mergedIdx] = nums1[i];
-            ++mergedIdx;
-            ++i;
-        }
-        else if (nums1[i] > nums2[j])
-        {
-            merged[mergedIdx] = nums2[j];
-            ++mergedIdx;
-            ++j;
-        }
-        else
-        {
-            merged[mergedIdx] = nums1[i];
-            merged[mergedIdx + 1] = nums2[j];
-            mergedIdx += 2;
-            ++i;
-            ++j;
-        }
-    }
-    while (i < m)
-    {
-        // Fill with remaining elements from nums1
-        merged[mergedIdx] = nums1[i];
-        ++mergedIdx;
-        ++i;
-    }
-    while (j < n)
-    {
-        // Fill with remaining elements from nums2
-        merged[mergedIdx] = nums2[j];
-        ++mergedIdx;
-        ++j;
-    }
+		size_t i = 0;
+		size_t j = 0;
+		size_t mergedIdx = 0;
+		while (i < m && j < n)
+		{
+			if (nums1[i] < nums2[j])
+			{
+				merged[mergedIdx] = nums1[i];
+				++mergedIdx;
+				++i;
+			}
+			else if (nums1[i] > nums2[j])
+			{
+				merged[mergedIdx] = nums2[j];
+				++mergedIdx;
+				++j;
+			}
+			else
+			{
+				merged[mergedIdx] = nums1[i];
+				merged[mergedIdx + 1] = nums2[j];
+				mergedIdx += 2;
+				++i;
+				++j;
+			}
+		}
+		while (i < m)
+		{
+			// Fill with remaining elements from nums1
+			merged[mergedIdx] = nums1[i];
+			++mergedIdx;
+			++i;
+		}
+		while (j < n)
+		{
+			// Fill with remaining elements from nums2
+			merged[mergedIdx] = nums2[j];
+			++mergedIdx;
+			++j;
+		}
 
-    nums1 = std::move(merged);
+		nums1 = std::move(merged);
+	}
+}
+
+// https://leetcode.com/problems/same-tree/
+namespace p100
+{
+	void dfs(TreeNode* node, std::string& repr)
+	{
+		if (!node)
+		{
+			repr += 'N';
+			return;
+		}
+
+		repr += std::to_string(node->val);
+		dfs(node->left, repr);
+		dfs(node->right, repr);
+	}
+
+	bool isSameTree(TreeNode* p, TreeNode* q)
+	{
+		std::string pString{};
+		std::string qString{};
+		dfs(p, pString);
+		dfs(q, qString);
+
+		return pString == qString;
+	}
+
+	// Other solution
+	bool isSameTree2(TreeNode* p, TreeNode* q)
+	{
+		if (!p && !q)
+			return true;
+
+		if (!p || !q || p->val != q->val)
+			return false;
+
+		return isSameTree2(p->left, q->left) && isSameTree2(p->right, q->right);
+	}
+}
+
+// https://leetcode.com/problems/symmetric-tree/
+namespace p101
+{
+	bool isSymmetricDFS(TreeNode* p, TreeNode* q)
+	{
+		if (!p && !q)
+			return true;
+
+		if (!p || !q || p->val != q->val)
+			return false;
+
+		return isSymmetricDFS(p->left, q->right) && isSymmetricDFS(p->right, q->left);
+	}
+
+	bool isSymmetric(TreeNode* root)
+	{
+		return isSymmetricDFS(root->left, root->right);
+	}
+}
+
+// https://leetcode.com/problems/symmetric-tree/
+namespace p104
+{
+	int maxDepth(TreeNode* root) 
+	{
+		if (!root)
+			return 0;
+
+		int leftHeight = 1 + maxDepth(root->left);
+		int rightHeight = 1 + maxDepth(root->right);
+		return std::max(leftHeight, rightHeight);
+	}
+}
+
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+namespace p108
+{
+	TreeNode* buildTree(const std::vector<int>& nums, int left, int right)
+	{
+		if (left > right)
+			return nullptr;
+
+		int mid = (right - left) / 2 + left;
+		TreeNode* root = new TreeNode(nums[mid]);
+		root->left = buildTree(nums, left, mid - 1);
+		root->right = buildTree(nums, mid + 1, right);
+		return root;
+	}
+
+	TreeNode* sortedArrayToBST(const std::vector<int>& nums) 
+	{
+		TreeNode* root = buildTree(nums, 0, nums.size() - 1);
+		return root;
+	}
 }
 
 int main()
 {
-    std::vector nums1{ 2, 0 };
-    std::vector nums2{ 1 };
-
-    merge(nums1, 1, nums2, 1);
+	const std::vector nums{ -10, -3, 0, 5, 9 };
+	TreeNode* tree = p108::sortedArrayToBST(nums);
 
 }
